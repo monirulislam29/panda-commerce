083d3865f2ac2721ba092456043225f4cbb42129
monirulislam29 <monirulislam284655@gmail.com>
2022-07-23 23:02:04 +0600

    add prctice

---
+++ panda-commerce/panda.js
@@ -0,0 +1,15 @@
+// function showJs(){
+// 	console.log('i am working on behalf of onclick funcation');
+// }
+// document.getElementById('backpack').style.backgroundColor='tomato';
+// 		const cards=document.getElementsByClassName('card');
+// 		for(card of cards){
+// 			card.style.borderRadius='30px';
+// 		}
+// 		cards.style.borderRadius='30px';
+
+// 		document.getElementsByClassName('button').addEventListener('click',function(event){
+// 			event.target.parentNode.removeChild(event.target)
+// 		});
+	
+
